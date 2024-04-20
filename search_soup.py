4d29725c5140e5f131e51656b0cd71aaed1b07fd
Aimilia Dimitra Ktena <t8210073@aueb.gr>
2024-04-20 13:12:42 +0300

    Retrieve url links of search result and put them in a list

--- search_soup.py
+++ search_soup.py
@@ -4,6 +4,14 @@ import requests
 url = 'https://www.kathimerini.gr/search/nova%20united%20group/'
 page = requests.get(url)
 
-extract = BeautifulSoup(page.text, 'html')
+soup = BeautifulSoup(page.content, 'html.parser')
+links = soup.find_all('a', href=True)
 
-print(extract)
+article_urls = []
+for link in links:
+    # Check if the link points to an article based on its URL pattern
+    if 'pages/announcements/' in link['href']:
+        article_urls.append(link['href'])
+
+article_urls = list(set(article_urls))
+print(article_urls)

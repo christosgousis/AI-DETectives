45db4fd09284b43ceec91f020624ffd9a552b800
Aimilia Dimitra Ktena <t8210073@aueb.gr>
2024-04-21 00:25:49 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,13 +6,18 @@ customtkinter.set_default_color_theme("blue")
 
 def toggle_combobox():
     if checkbox_var.get() == 1:
-        combobox_frame.pack(pady=10)
+        combobox_frame.pack(pady=(5, 10))  
+        output_text.place(relx=0.5, rely=0.7, anchor="center")  # Position the output text label below the combobox
     else:
         combobox_frame.pack_forget()
+        output_text.place_forget()
 
 def search():
     keywords = entry.get()  # Get keywords from the entry widget
-    output_text.configure(text=f"Searching for: {keywords}")  # Update the output text
+    if keywords:
+        output_text.configure(text=f"Searching for: {keywords}")  # Update the output text
+    else:
+        output_text.configure(text="")  # Clear the output text if no keywords are entered
 
 root = customtkinter.CTk()
 root.geometry("600x500")
@@ -35,19 +40,17 @@ search_button = customtkinter.CTkButton(master=root, text="Search", corner_radiu
 search_button.pack(pady=10)
 search_button.configure(width=50, height=20)
 
-# Checkbox
 checkbox_var = IntVar()
 checkbox = customtkinter.CTkCheckBox(master=root, text="I want to refine my search", variable=checkbox_var, command=toggle_combobox, font=("Helvetica", 13, "bold"))
-checkbox.pack(pady=10)
+checkbox.pack(pady=(10, 0))  
 
 combobox_frame = customtkinter.CTkFrame(master=root)
 
-# drop-down menu
 combobox = customtkinter.CTkComboBox(master=combobox_frame, values=["Marketing", "Human Resources", "IT", "Sales"])
 combobox.pack(pady=10)
 combobox.configure(width=300, height=30)
 
 output_text = customtkinter.CTkLabel(master=root, text="", wraplength=400)
-output_text.pack(pady=10)
+output_text.pack_forget()
 
 root.mainloop()
